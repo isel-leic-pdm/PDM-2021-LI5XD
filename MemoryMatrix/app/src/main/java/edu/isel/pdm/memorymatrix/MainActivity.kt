e190a6e8d362c6b73d01d674396c2cf87b799aa1
Paulo Pereira <palbp.giga@gmail.com>
2020-10-19 14:54:05 +0100

    Updates kotlin language version (and associated build tools)

--- MemoryMatrix/app/src/main/java/edu/isel/pdm/memorymatrix/MainActivity.kt
+++ MemoryMatrix/app/src/main/java/edu/isel/pdm/memorymatrix/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.startButton).setOnClickListener {
             Log.v("APP_TAG", "Button clicked")
