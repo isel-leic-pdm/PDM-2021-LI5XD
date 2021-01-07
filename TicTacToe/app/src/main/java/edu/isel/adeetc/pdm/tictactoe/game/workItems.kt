2def1b1310254aa4fd324a37b83e75a34283eded
Paulo Pereira <palbp.giga@gmail.com>
2021-01-07 12:55:14 +0000

    Removes incomplete implementation of CustomListenableFuture

--- TicTacToe/app/src/main/java/edu/isel/adeetc/pdm/tictactoe/game/workItems.kt
+++ TicTacToe/app/src/main/java/edu/isel/adeetc/pdm/tictactoe/game/workItems.kt
@@ -1,9 +1,7 @@
 package edu.isel.adeetc.pdm.tictactoe.game
 
 import android.content.Context
-import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import androidx.concurrent.futures.CallbackToFutureAdapter
 import androidx.work.ListenableWorker
 import androidx.work.Worker
@@ -11,8 +9,8 @@ import androidx.work.WorkerParameters
 import com.google.common.util.concurrent.ListenableFuture
 import edu.isel.adeetc.pdm.tictactoe.TAG
 import edu.isel.adeetc.pdm.tictactoe.TicTacToeApplication
-import java.lang.IllegalStateException
-import java.util.concurrent.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 const val CHALLENGE_ID_KEY = "CHALLENGE_ID"
 
@@ -81,74 +79,5 @@ class AsyncGameStateCleanup(
                 }
             )
         }
-    }
-}
-
-/**
- * A demo custom implementation, just for fun
- */
-@RequiresApi(Build.VERSION_CODES.N)
-class CustomListenableFuture<T> : ListenableFuture<T> {
-
-    private val done = CountDownLatch(1)
-    private var result: T? = null
-
-    private data class Listener(val listener: Runnable, val executor: Executor)
-
-    private val listeners = ConcurrentLinkedQueue<Listener>()
-
-    override fun addListener(listener: Runnable, executor: Executor) {
-        listeners.add(Listener(listener, executor))
-    }
-
-    override fun cancel(mayInterruptIfRunning: Boolean) = false
-    override fun isCancelled() = false
-    override fun isDone() = done.count == 0L
-
-    override fun get(): T {
-        done.await()
-        return result ?: throw IllegalStateException()
-    }
-
-    override fun get(timeout: Long, unit: TimeUnit?): T {
-        done.await(timeout, unit)
-        return result ?: throw IllegalStateException()
-    }
-
-    fun set(result: T) {
-        if (isDone) throw IllegalStateException()
-        this.result = result
-        done.countDown()
-    }
-}
-
-/**
- * An asynchronous implementation using a callback to ListenableFuture custom adaptation.
- */
-@RequiresApi(Build.VERSION_CODES.N)
-class AnotherAsyncGameStateCleanup(
-    ctx: Context,
-    params: WorkerParameters
-) : ListenableWorker(ctx, params) {
-
-    override fun startWork(): ListenableFuture<Result> {
-
-        val challengeId = inputData.getString(CHALLENGE_ID_KEY) ?: ""
-        logWithThreadInfo("Cleaning up of $challengeId")
-
-        val result = CustomListenableFuture<Result>()
-        (applicationContext as TicTacToeApplication).repository.deleteGame(
-            challengeId,
-            {
-                logWithThreadInfo("Cleanup of $challengeId succeeded")
-                result.set(Result.success())
-            },
-            {
-                logWithThreadInfo("Cleanup of $challengeId failed")
-                result.set(Result.retry())
-            }
-        )
-
-        return result
     }
 }
